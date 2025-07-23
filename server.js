2a08a4f7d32aabc6516694dc3a6785d460b0628d
mudiagaobriki <38283045+mudiagaobriki@users.noreply.github.com>
2025-07-23 10:43:05 +0100

    minor fix

--- server.js
+++ server.js
@@ -45,10 +45,12 @@ const limiter = rateLimit({
 app.use(limiter);
 
 // CORS configuration
-app.use(cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
-    credentials: true
-}));
+// app.use(cors({
+//     origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+//     credentials: true
+// }));
+
+app.use(cors())
 
 // Body parsing middleware
 app.use(express.json({ limit: '50mb' }));
